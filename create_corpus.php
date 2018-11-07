047aad190eb335b72eca61deb2e3b53028a3e800
Mason Pawsey 🇦🇺 <mason.pawsey@yahoo.com>
2018-11-07 04:50:07 +0000

    parse for quotes

--- create_corpus.php
+++ create_corpus.php
@@ -8,25 +8,21 @@ $user = 'phpmyadmin';
 $pass = '25dba36cbfa5b0a17a03a7fb8e10c96496de6d99b5459fc2';
 $dbh = new PDO('mysql:host=localhost;dbname=tweets', $user, $pass);
 
-$statement = $dbh->prepare('SELECT sentiment, text FROM tweets WHERE sentiment IS NOT NULL');
+$statement = $dbh->prepare('SELECT sentiment, text FROM tweets WHERE sentiment IS NOT NULL LIMIT 100');
 $statement->execute();
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 $file = fopen("corpus.csv", "w") or die("Unable to open file!");
 chmod(__DIR__."/corpus.csv", 0777);
 foreach ($results as $key => $value) {
-	$text =  "\"".$value['sentiment']."\",\"".trim($value['text'])."\"\n";
+	$text =  "\"".$value['sentiment']."\",\"".str_replace('"', "'", $value['text'])."\"\n";
 	fwrite($file, $text);
 }
-
 
 header('Content-type: application/pdf');
 header('Content-Disposition: attachment; filename="' . basename("corpus.csv") . '"');
 header('Content-Transfer-Encoding: binary');
 readfile("corpus.csv");
-
-echo "File downloaded!\n";
-
 fclose($file);
 
 ?>
