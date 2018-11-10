4dea7a0d44a5be37d9f1c0007dc1748f43bf27bc
Mason Pawsey 🇦🇺 <mason.pawsey@yahoo.com>
2018-11-10 03:31:56 +0000

    work on highlighting keyword

--- search.php
+++ search.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+
+if(empty($_POST)) {
+	header("Location: ../");
+	die();
+}
+
 ?>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
@@ -74,8 +80,13 @@ $file_id = uniqid();
 // (and because ./scrape-twitter is protected and www-data can't get to it)
 $raw = shell_exec("sudo /var/www/html/run.sh $latitude $longitude $keyword");
 
-// This finds our keyword in the result, gives it a <span> tag to highlight it, then prints it
-print_r(json_decode(preg_replace("/\p{L}*?".preg_quote($keyword)."\p{L}*/ui", "<span class='highlight'>$0</span>", $raw), true));
+$keyword = $_POST['keyword'];
+// Check for the keyword
+$result = preg_replace("/\p{L}*?".preg_quote($keyword)."\p{L}*/ui", "<span class='highlight'>$0</span>", $raw);
+// Check for the keyword without spaces
+$keyword = str_replace(' ', '', $keyword);
+$result = preg_replace("/\p{L}*?".preg_quote($keyword)."\p{L}*/ui", "<span class='highlight'>$0</span>", $raw);
+print_r(json_decode($result, true));
 
 $dbh = null;
 
