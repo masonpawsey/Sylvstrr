c10c148a708a988e64a96c568111b44631d76f93
Mason Pawsey 🇦🇺 <mason.pawsey@yahoo.com>
2018-12-10 07:54:36 +0000

    add profile page

---
+++ profile.php
@@ -0,0 +1,120 @@
+<?php
+session_start();
+require_once 'vendor/autoload.php';
+
+use PHPAuth\Config as PHPAuthConfig;
+use PHPAuth\Auth as PHPAuth;
+
+require_once('credentials.php');
+$dbh = new PDO('mysql:host=localhost;dbname=tweets', $user, $pass);
+
+$config = new PHPAuthConfig($dbh);
+$auth = new PHPAuth($dbh, $config);
+
+if (!$auth->isLogged()) {
+	header("Location: index.php");
+	die('Forbidden');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Sylvester</title>
+    <!-- Boostrap CSS & JS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.13/js/mdb.min.js"></script>
+    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.50.0/mapbox-gl.js'></script>
+    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.50.0/mapbox-gl.css' rel='stylesheet' />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.13/css/mdb.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="home-style.css">
+    <script type="text/javascript" src="cities.js"></script>
+</head>
+
+<body>
+    <div id="wrapper">
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+
+                    </a>
+                </li>
+                <li>
+                    <a href="home.php">Dashboard</a>
+                </li>
+                <li>
+                    <a href="history.php">Query history</a>
+                </li>
+                <li>
+                    <a href="profile.php">Profile</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <header>
+                <!-- Fixed navbar -->
+                <nav class="navbar navbar-expand fixed-top">
+                    <a class="navbar-brand" href="#">sylvester</a>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#menu-toggle" id="menu-toggle"><i class="fas fa-bars"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <img class="rounded-circle" style="width:35px; margin: 0 10px;" src="https://randomuser.me/api/portraits/men/<?php echo rand(0,10);?>.jpg" alt="">
+                  <li class="nav-item dropdown" style="list-style: none;">
+                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <?php echo $auth->getCurrentUser()['email']; ?>
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Profile</a>
+                            <a class="dropdown-item" href="logout.php">Log out</a>
+                          </div>
+                        </li>
+                </nav>
+            </header>
+            <!-- Begin page content -->
+            <main role="main" class="container-fluid">
+                <div class="row" style="margin-top: 60px">
+                    <div class="col-sm-8">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title"><strong>Welcome, <?php echo $auth->getCurrentUser()['email']; ?></strong></h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+        <!-- /#page-content-wrapper -->
+        <!-- /#wrapper -->
+        <footer class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                  <div class="col-6 text-right">Team Sylvester</div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <script>
+    $(document).ready(function() {
+        
+    });
+    </script>
+</body>
+
+</html>
