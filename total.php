367955e839cd008ccdc6fd0caf2c12a0d3016e0f
Mason Pawsey 🇦🇺 <mason.pawsey@yahoo.com>
2019-02-13 21:55:54 +0000

    update total page to include db size

--- total.php
+++ total.php
@@ -1,4 +1,4 @@
-<?php
+<pre><?php
 
 // Connect to the database
 $user = 'phpmyadmin';
@@ -8,10 +8,11 @@ $dbh = new PDO('mysql:host=localhost;dbname=tweets', $user, $pass);
 // Count the positive Tweets
 $statement = $dbh->prepare('SHOW TABLE STATUS');
 $statement->execute();
-$result = $statement->fetchAll(PDO::FETCH_ASSOC)[8]['Rows'];
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+$result = $results[8]['Rows'];
 
 exec("ls /var/www/html/trainer/jsons | wc -l 2>&1", $output, $return_var);
 
-echo "Current: " . number_format($result) . "<br>Pending JSONs: " . $output['0'] . "<br>Estimated: +" . number_format($output[0]*1150) . "<br>Assumed total: " . number_format($result + $output[0]*1150);
+echo "Current: " . number_format($result) . "<br>Pending JSONs: " . $output['0'] . "<br>Estimated: +" . number_format($output[0]*1150) . "<br>Assumed total: " . number_format($result + $output[0]*1150)."<br>Current size: " . number_format($results[8]['Data_length']/(1024*1024*1024),2) . "gb";
 
-?>
+?></pre>
